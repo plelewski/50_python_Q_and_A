5edd933b38e1b84f128c952764c0e59cf91013f5
Przemek Lelewski <peklel@wp.pl>
2021-01-25 22:36:29 +0100

    kolejne pliki

---
+++ 33_generatory.py
@@ -0,0 +1,26 @@
+# Pytanie 33 - do czego w Pythonie służy słowo kluczowe yield ?
+# Napisz przykładowy kod wykorzystujący yield.
+
+def zwracaj_kolejne_parzyste():        # definicja generatora wygląda jak definicja zwykłej funkcji
+    for n in range(2,20,2):            # range tworzący zakres od 2 do 20, przesuwając się o 2
+        yield n                        # słowo yield informuje interpreter, że ta funkcja będzie generatorem
+
+z = zwracaj_kolejne_parzyste()         # tworzenie obiektu generatora
+
+for i in range(9):                    # pętla for wykonująca się 10 razy
+    print(next(z))                     # która za kazdym razem drukuje kolejną wartość zwróconą z obiektu genratora z
+
+y = ('a' * n for n in range(5))        # generator expression - wyrażenie generatorowe
+                                       # tworzy generator, który będzie zwracał kolejne wielokrotności stringa 'a'
+                                       # zakresie od 0 do 4
+
+for i in range(5):                     # wypisanie kolejnych wartości zwróconych przez obiekt genratora y
+# zamiast i można użyć znaku "_", ponieważ zmienna i nie jest używana i python czegoś w tym miejscu wymaga
+    print(next(y))
+
+# zadanie dodatkowe
+
+x = (5 * n for n in range(1,11))
+
+for _ in range(10):
+    print(next(x))
